b1e35a575584f4c09be6082bb329f1141a3af11a
dsyleixa <29887867+dsyleixa@users.noreply.github.com>
2019-08-04 09:38:54 +0200

    Add files via upload

---
+++ pthreadprogs/pthread_cpus.c
@@ -0,0 +1,29 @@
+// run program on reserved cpus 2+3
+//
+// add in /boot/cmdline.txt :
+// isolcpus= 2,3
+// https://www.raspberrypi.org/forums/viewtopic.php?f=33&t=247493&p=1512885#p1512885
+
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sched.h>
+
+int main()
+{
+  cpu_set_t cpus;
+
+  CPU_ZERO(&cpus);
+
+  CPU_SET(2, &cpus);
+  CPU_SET(3, &cpus);
+
+  if (sched_setaffinity(0, sizeof(cpus), &cpus)) {
+    perror("sched_setaffinity");
+    exit(1);
+  }
+  
+  /* Do stuff */
+}
