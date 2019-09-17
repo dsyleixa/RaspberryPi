16247405e37d9d4c6289a908831a8c185da8dcab
dsyleixa <29887867+dsyleixa@users.noreply.github.com>
2019-09-17 18:06:42 +0200

    Add files via upload

--- qt/quit_wiringPi/mainwindow.h
+++ qt/quit_wiringPi/mainwindow.h
@@ -2,21 +2,32 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+
 
 namespace Ui {
 class MainWindow;
 }
+
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_highButton_clicked();
+    void on_lowButton_clicked();
+    void on_quitButton_clicked();
+    void onUpdateTime();
 
 private:
     Ui::MainWindow *ui;
+    int pin23, pin24;
+    QTimer updateTimer;
 };
 
 #endif // MAINWINDOW_H
