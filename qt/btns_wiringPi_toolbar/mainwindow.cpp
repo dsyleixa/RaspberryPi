303134256ad9b1f835cf7d90d71c1b5dcfefeee6
dsyleixa <29887867+dsyleixa@users.noreply.github.com>
2019-10-03 18:07:17 +0200

    Update mainwindow.cpp

--- qt/btns_wiringPi_toolbar/mainwindow.cpp
+++ qt/btns_wiringPi_toolbar/mainwindow.cpp
@@ -3,20 +3,21 @@
 #include <wiringPi.h>
 
 
-/*
  * GPIO setup (BCM numbering):
- * 23: Output (green LED + resistor)
+ * 23: Output (green LED + resistor) // switchable by widget buttons)
  * 24: Input  (default: GPIO24->switch->GND)
- * 25: Output (red LED + resistor)
+ * 25: Output (red LED + resistor)  // LED is lit while program runs
  *
  * Input 24 can be switched from INPUT_PULLUP (default) to INPUT_PULLDOWN
- * (beyond toolbar option Edit)
+ * (beneath toolbar option Edit)
  * when  INPUT_PULLDOWN  is activated, the switch has to be re-wired
  * (GPIO24->switch->+3v3)
  *
 */
 
-MainWindow::MainWindow(QWidget *parent)
+
+
+nWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
