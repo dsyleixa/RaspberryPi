48fc3a904db3e3a2363514239f6fb40bb2464822
dsyleixa <29887867+dsyleixa@users.noreply.github.com>
2019-10-03 20:01:01 +0200

    Update mainwindow.cpp

--- qt/btns_wiringPi_toolbar_pthread/mainwindow.cpp
+++ qt/btns_wiringPi_toolbar_pthread/mainwindow.cpp
@@ -10,7 +10,7 @@
 * 25: Output (red LED + resistor)  // LED is blinking while program runs
 *
 * Input 24 can be switched from INPUT_PULLUP (default) to INPUT_PULLDOWN
-* (beyond toolbar option Edit)
+* (beneath toolbar option Edit)
 * when  INPUT_PULLDOWN  is activated, the switch has to be re-wired
 * (GPIO24->switch->+3v3)
 *
