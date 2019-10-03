ba4b318b911235887facf833f6cbb4438fbf7bfa
dsyleixa <29887867+dsyleixa@users.noreply.github.com>
2019-10-03 17:57:20 +0200

    Update mainwindow.cpp

--- qt/btns_wiringPi_toolbar/mainwindow.cpp
+++ qt/btns_wiringPi_toolbar/mainwindow.cpp
@@ -3,6 +3,19 @@
 #include <wiringPi.h>
 
 
+/*
+ * GPIO setup (BCM numbering):
+ * 23: Output (green LED + resistor)
+ * 24: Input  (default: GPIO24->switch->GND)
+ * 25: Output (red LED + resistor)
+ *
+ * Input 24 can be switched from INPUT_PULLUP (default) to INPUT_PULLDOWN
+ * (beyond toolbar option Edit)
+ * when  INPUT_PULLDOWN  is activated, the switch has to be re-wired
+ * (GPIO24->switch->+3v3)
+ *
+*/
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -14,8 +27,16 @@ MainWindow::MainWindow(QWidget *parent)
         exit(1);
 
     pinMode(23, OUTPUT);
+    digitalWrite(23, LOW); // init, default
+    pin23 = LOW;
+    ui->pin23Label->setText(QString("Low"));
+
+    pinMode(25, OUTPUT);
+    digitalWrite(25, HIGH); // init, default
+
     pinMode(24, INPUT);
-    pullUpDnControl(24, PUD_UP);
+    pullUpDnControl(24, PUD_UP); // init, default
+
 
     //on_lowButton_clicked();// Slots can be called also within the program
     onUpdateTime();
@@ -33,6 +54,7 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow() {
     updateTimer.stop();
     digitalWrite(23, LOW);
+    digitalWrite(25, LOW);
     delete ui;
 }
 
@@ -64,6 +86,7 @@ MainWindow::on_lowButton_clicked() {
 void __attribute__((noreturn))
 MainWindow::on_quitButton_clicked() {
     digitalWrite(23, LOW);
+    digitalWrite(25, LOW);
     exit(EXIT_SUCCESS);
 }
 
@@ -71,17 +94,21 @@ MainWindow::on_quitButton_clicked() {
 void MainWindow::on_actionQuit_triggered()
 {
     //ui->statusBar->showMessage("File Quit menu activated", 2000);
+    digitalWrite(23, LOW);
+    digitalWrite(25, LOW);
     exit(EXIT_SUCCESS);
 }
 
 
 void MainWindow::on_actionGPIO24_pullup_triggered()
 {
-    ui->statusBar->showMessage("Test for GPIO pullup activated (still unfunctional)", 2000);
+    ui->statusBar->showMessage("Test for GPIO pullup activated ", 1000);
+    pullUpDnControl(24, PUD_UP);
 }
 
 
 void MainWindow::on_actionGPIO24_pulldown_triggered()
 {
-    ui->statusBar->showMessage("Test for GPIO pulldown activated (still unfunctional)", 2000);
+    ui->statusBar->showMessage("Test for GPIO pulldown activated ", 1000);
+    pullUpDnControl(24, PUD_DOWN);
 }
