03f6e3659cc03f04ceb6194ee70cd7cb3e2b57f1
dsyleixa <29887867+dsyleixa@users.noreply.github.com>
2020-06-10 14:00:24 +0200

    Add files via upload

---
+++ qt/btn_paint_timer/mainwindow.cpp
@@ -0,0 +1,69 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //--------------------------------------------------------------
+    // Timer
+    QTimer updateTimer;
+    //onUpdateTime(); // see: mainwindow.h
+    // Every time the Timer will reach its terminal count
+    // invoke the "slot" onUpdateTime()
+    connect(&updateTimer, SIGNAL(timeout()), this, SLOT(onUpdateTime()));
+    // Start the timer
+    updateTimer.start(50);// in msec
+
+ }
+
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    close();
+}
+
+
+
+
+// Invoked every 50ms
+void
+MainWindow::onUpdateTime() {
+    //--------------------------------------------------------------
+    // Heater: Create a GraphicsScene and add it to the graphicsView
+    scene = new QGraphicsScene(this);
+    ui->graphicsView->setScene(scene);
+    int r,s,x,y;
+
+    // Heater: Create a blue and green brushes and outline pen
+    QBrush blueBrush(Qt::blue);
+    QBrush greenBrush(Qt::green);
+    QPen outlinePen(Qt::black);
+    outlinePen.setWidth(2);
+
+    // Heater: Add a blue rectabgle to our graphics scene.
+    x=rand()%200;
+    y=rand()%200;
+    rectangle = scene->addRect(0, 0, x, y, outlinePen, blueBrush);
+
+    // Heater: Add a green elipse to our graphics scene.
+    // addEllipse(x,y,w,h,pen,brush)
+    ellipse = scene->addEllipse(50, 50, 300, 60, outlinePen, greenBrush);
+
+    // Heater: Add a some text to our graphics scene.
+    text = scene->addText("Hi dsyleixa123!", QFont("Arial", 20) );
+
+    // Heater: Add a line
+    line = scene->addLine(250, 0, 200, 200, outlinePen);
+    line->setPos(0, 0);
+
+
+}
---
+++ qt/btn_paint_timer/mainwindow.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTimer>
+#include <QGraphicsLineItem>
+#include <QGraphicsScene>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+    void onUpdateTime();
+
+private:
+    Ui::MainWindow *ui;
+
+    // Heater: We are going to keep our grphics scene and some graphics items here.
+    QGraphicsScene *scene;
+    QGraphicsEllipseItem *ellipse;
+    QGraphicsRectItem *rectangle;
+    QGraphicsTextItem *text;
+    QGraphicsLineItem *line;
+};
+#endif // MAINWINDOW_H
