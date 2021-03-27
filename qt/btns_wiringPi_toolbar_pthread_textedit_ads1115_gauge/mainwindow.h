5d247961e282b14b0818db6d4f2c9d8cd2ef6640
dsyleixa <29887867+dsyleixa@users.noreply.github.com>
2021-03-27 09:26:52 +0100

    Add files via upload

---
+++ qt/btns_wiringPi_toolbar_pthread_textedit_ads1115_gauge/mainwindow.h
@@ -0,0 +1,56 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QApplication>
+#include <QTimer>
+#include <QGraphicsLineItem>
+#include <QGraphicsEllipseItem>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+
+
+
+namespace Ui {
+class MainWindow;
+}
+
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+
+private slots:
+    void onUpdateTime();
+    void on_highButton_clicked();
+    void on_lowButton_clicked();
+    void on_quitButton_clicked();    
+    void on_actionQuit_triggered();
+    void on_actionGPIO25_PUP_triggered();
+    void on_actionGPIO25_PDN_triggered();
+
+private:
+    Ui::MainWindow *ui;
+    QTimer updateTimer;
+
+    QGraphicsScene *scene0;
+    QGraphicsScene *scene1;
+    QGraphicsScene *scene2;
+    QGraphicsScene *scene3;
+
+    QGraphicsEllipseItem *ellipse;
+    QGraphicsRectItem *rectangle;
+    QGraphicsTextItem *text;
+    QGraphicsLineItem *line;
+    QGraphicsLineItem *pline0;
+    QGraphicsLineItem *pline1;
+    QGraphicsLineItem *pline2;
+    QGraphicsLineItem *pline3;
+};
+
+#endif // MAINWINDOW_H
