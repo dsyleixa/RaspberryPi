939276038ee2312332a63f0df9c149a4bab7de53
dsyleixa <29887867+dsyleixa@users.noreply.github.com>
2021-05-21 11:08:38 +0200

    Add files via upload

---
+++ qt/btns_wiringPi/mainwindow.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTimer>
+
+
+namespace Ui {
+class MainWindow;
+}
+
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_highButton_clicked();
+    void on_lowButton_clicked();
+    void on_quitButton_clicked();
+    void onUpdateTime();
+
+    void on_quitButton_clicked(bool checked);
+
+private:
+    Ui::MainWindow *ui;
+    int pin23, pin24;
+    QTimer updateTimer;
+};
+
+#endif // MAINWINDOW_H
---
+++ qt/btns_wiringPi/readme.txt
@@ -0,0 +1,6 @@
+/*
+ * GPIO setup (BCM numbering):
+ * 23: Output (LED + resistor)
+ * 24: Input  (default: GPIO=input_pullup, wiring: GPIO24->switch->GND)
+ *
+*/
