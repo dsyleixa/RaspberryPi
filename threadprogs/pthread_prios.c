bae6a12a8d4c4609becbb7c5dbf4d4211295e4dd
dsyleixa <29887867+dsyleixa@users.noreply.github.com>
2021-12-01 08:40:11 +0100

    Add files via upload

---
+++ threadprogs/pthread_prios.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+
+volatile static uint32_t  TimerTick=0;
+
+void ISR(uint32_t tick) {
+   // do sth specific
+}
+
+
+uint32_t timerloop() {
+   while(true) {
+      if(TimerTick>=100000l)TimerTick=0;
+      ISR (TimerTick);
+      TimerTick++;
+      delayMicroseconds(10);
+   }
+}
+
+
+int main() {
+   const int policy = SCHED_RR;
+   const int minPrio = sched_get_priority_min(policy);
+   const int maxPrio = sched_get_priority_max(policy);
+   sched_param param;
+   
+   pthread thread0;
+   pthread_create(&thread0, NULL, timerloop, NULL);
+   param.sched_priority = maxPrio - 1;
+   pthread_setschedparam(thread0, policy, &param);
+
+   //...
+  
+}
